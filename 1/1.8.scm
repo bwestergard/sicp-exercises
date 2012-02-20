d49714e0da1e54cc47e875dd6094c72a46354cc2
Bjorn Westergard <bjornw@gmail.com>
2012-02-20 15:50:17 -0500

    1.8
    Signed-off-by: Bjorn Westergard <bjornw@gmail.com>

---
+++ 1/1.8.scm
@@ -0,0 +1,23 @@
+(define (cbrt-iter lastguess guess x)
+  (if (good-enough? lastguess guess x)
+      guess
+      (cbrt-iter guess (improve guess x) x)))
+
+(define (improve y x)
+  (/ (+ (/ x (square y)) (* 2 y)) 3))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? lastguess guess x)
+  (< (/ (abs (- lastguess guess)) x) 0.0001))
+
+(define (cbrt x)
+  (cbrt-iter (* 4 x) x x))
+
+(define (cube x)
+  (* (* x x) x))
+
+(define (square x)
+  (* x x))
+
