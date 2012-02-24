04f5b2ca6826baab111dd68c37bc4e16e2108efa
Bjorn Westergard <bjornw@gmail.com>
2012-02-24 00:48:49 -0500

    1.16
    Signed-off-by: Bjorn Westergard <bjornw@gmail.com>

---
+++ 1/1.16/1.16.scm
@@ -0,0 +1,16 @@
+(define (fast-expt a b n) ; This is the given recursive solution.
+  (cond ((= n 0) 1)
+        ((even? n) (square (fast-expt a b (/ n 2))))
+        (else (* b (fast-expt a b (- n 1))))))
+
+(define (expi b n a) ; Iterative version.
+  (cond ((= n 0) a)
+  ((even? n) (expi (square b) (- (/ n 2) 1) (* (square b) a))) ; Kind of leaning on the edge case (n = 0) behavior here.
+  (else (expi b (- n 1) (* b a)))
+  )
+)
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (square n) (* n n))
