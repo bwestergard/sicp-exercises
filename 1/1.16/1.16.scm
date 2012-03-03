4845bbf7c43e62ba927a57b562f53dff83adc6b1
Bjorn Westergard <bjornw@gmail.com>
2012-03-03 01:43:11 -0500

    1.17 added, small change to 1.16 with no real impact on running time.
    Signed-off-by: Bjorn Westergard <bjornw@gmail.com>

--- 1/1.16/1.16.scm
+++ 1/1.16/1.16.scm
@@ -5,7 +5,7 @@
 
 (define (expi b n a) ; Iterative version.
   (cond ((= n 0) a)
-  ((even? n) (expi (square b) (- (/ n 2) 1) (* (square b) a))) ; Kind of leaning on the edge case (n = 0) behavior here.
+  ((even? n) (expi (square b) (/ n 2) a)) ; Kind of leaning on the edge case (n = 0) behavior here.
   (else (expi b (- n 1) (* b a)))
   )
 )
