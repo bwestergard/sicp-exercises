5557cabe448d1c3169339b97e033dd5131516069
Bjorn Westergard <bjornw@gmail.com>
2012-07-18 23:49:08 -0400

    An even terser version of make-rat in 2.1. Hit upon this with the group yesterday.
    Signed-off-by: Bjorn Westergard <bjornw@gmail.com>

--- 2/2.1/2.1.scm
+++ 2/2.1/2.1.scm
@@ -1,8 +1,9 @@
 (define (make-rat n d)
-  (let ((g (gcd n d)))
-    (if (< d 0)
-      (cons (* -1 (/ n g)) (* -1 (/ d g)))
-      (cons (/ n g) (/ d g)))))
+  (let ((g (* 
+            (gcd n d)
+            (if (< d 0) -1 1))))
+    (cons (/ n g) (/ d g))
+    ))
 
 (define (numer x) (car x))
 
@@ -15,3 +16,5 @@
   (display (denom x)))
 
 (print-rat (make-rat -26 -420))
+(print-rat (make-rat -26 420))
+(print-rat (make-rat 26 -420))
