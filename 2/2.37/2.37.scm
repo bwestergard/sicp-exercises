232682a37f06110cfd03ff4269a880f7e1d9e912
Bjorn Westergard <bjorn@helmholtz.(none)>
2013-03-01 00:25:09 -0500

    2.37
    Signed-off-by: Bjorn Westergard <bjorn@helmholtz.(none)>

---
+++ 2/2.37/2.37.scm
@@ -0,0 +1,28 @@
+(define mat-x '((1 2 3 4) (4 5 6 7) (7 8 9 0)))
+(define vec-a (list-ref mat-x 0))
+(define vec-b (list-ref mat-x 1))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      nil
+      (cons (accumulate op init (map car seqs))
+            (accumulate-n op init (map cdr seqs)))))
+
+(define (dot-product v w)
+  (accumulate + 0 (map * v w)))
+
+(define (matrix-*-vector m v)
+  (map (lambda (row) (dot-product row v)) m))
+
+(define (transpose mat)
+  (accumulate-n cons '() mat))
+
+(define (matrix-*-matrix m n)
+  (let ((cols (transpose n)))
+    (map (lambda (row) (matrix-*-vector cols row)) m)))
